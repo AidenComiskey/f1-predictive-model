91f2f6d194384614c0e0ae05a0cee6adff8ea916
AidenComiskey <aiden.whisk@hotmail.com>
2025-08-12 19:27:57 +0100

    added code to get race results and tested

--- main.py
+++ main.py
@@ -2,3 +2,17 @@ import pandas as pd
 import fastf1 as ff
 
 ff.Cache.enable_cache('f1_cache')
+
+def get_race_and_quali(year, round_num):
+    race = ff.get_session(year, round_num, 'Race')
+    race.load()
+    race_results = race.results[['Position', 'Abbreviation', 'DriverNumber', 'TeamName', 'GridPosition']].copy()
+
+    race_results['Year'] = year
+    race_results['Round'] = round_num
+    race_results['Circuit'] = race.event.Location
+    
+    return race_results
+
+df = get_race_and_quali(2023, 1)
+print(df)
