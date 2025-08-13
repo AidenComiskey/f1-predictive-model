3e7731e96c4078799aeec19ab29e3307f4dc8c61
AidenComiskey <aiden.whisk@hotmail.com>
2025-08-13 19:16:53 +0100

    added get function for qualifying and tested

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import fastf1 as ff
 
 ff.Cache.enable_cache('f1_cache')
 
-def get_race_and_quali(year, round_num):
+# function to get results from race from specified year and round
+def get_race_results(year, round_num):
     race = ff.get_session(year, round_num, 'Race')
     race.load()
     race_results = race.results[['Position', 'Abbreviation', 'DriverNumber', 'TeamName', 'GridPosition']].copy()
@@ -14,5 +15,19 @@ def get_race_and_quali(year, round_num):
     
     return race_results
 
-df = get_race_and_quali(2023, 1)
+# functiion to get results from qualifying from specified year and round
+def get_quali_results(year, round_num):
+    quali = ff.get_session(year, round_num, 'Qualifying')
+    quali.load()
+    quali_results = quali.results[['Position', 'Abbreviation', 'DriverNumber', 'TeamName']].copy()
+
+    quali_results['Year'] = year
+    quali_results['Round'] = round_num
+    quali_results['Circuit'] = quali.event.Location
+
+    return quali_results
+
+#df = get_race_results(2023, 1)
+#print(df)
+df = get_quali_results(2023, 1)
 print(df)
