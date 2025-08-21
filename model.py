1c0372fd008e6f09caa152086b011f2a224dad66
AidenComiskey <aiden.whisk@hotmail.com>
2025-08-21 19:32:36 +0100

    tested model and received mae of 3.5 on first model

--- model.py
+++ model.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
 
 df = pd.read_csv("data/basic_combined_data_clean.csv")
 
@@ -25,3 +26,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 # build the model
 model = RandomForestRegressor(n_estimators=100, random_state=42)
 model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+mae = mean_absolute_error(y_test, y_pred)
+print(f"MAE: {mae}")
