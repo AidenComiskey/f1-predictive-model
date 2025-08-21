483cb50178cd029a6c84ac4bf171ebd592a2ef61
AidenComiskey <aiden.whisk@hotmail.com>
2025-08-21 18:31:31 +0100

    changed team names to be the same throughout the years to make it more consistent

--- analysis.py
+++ analysis.py
@@ -8,14 +8,44 @@ print(df.head())
 #print(df.info())
 print(df.isnull().sum())
 
-
+# drop duplicate columns, dont need team name etc twice
 df = df.drop(columns=["DriverNumber_quali", "TeamName_quali", "Circuit_quali"])
 
+# drop null values from the dataset
 print("before drop na", df.shape)
 df = df.dropna()
 print("after drop na", df.shape)
 
+# grid position 0.0 represents pit lane start in fastf1 api
+# so changing it to 21 shows it is worse than starting last and makes it more accurate
 df.loc[df["GridPosition"] == 0, "GridPosition"] = 21
 print(df["GridPosition"].value_counts().sort_index())
+
+# team names have changed through the years so need to be made the same throughout the dataset
+team_mapping = {
+    "Force India": "Aston Martin",
+    "Racing Point": "Aston Martin",
+    "Renault": "Alpine",
+    "Toro Rosso": "Racing Bulls",
+    "Red Bull Racing": "Red Bull Racing",
+    "McLaren": "McLaren",
+    "Ferrari": "Ferrari",
+    "Mercedes": "Mercedes",
+    "Williams": "Williams",
+    "Sauber": "Kick Sauber",
+    "Alfa Romeo": "Kick Sauber",
+    "Aston Martin": "Aston Martin",
+    "Alpine": "Alpine",
+    "AlphaTauri": "Racing Bulls",
+    "Haas F1 Team": "Haas F1 Team",
+    "Kick Sauber": "Kick Sauber",
+    "RB": "Racing Bulls",
+    "Alfa Romeo Racing": "Kick Sauber"
+}
+
+# replace all team names with the ones from the dictionary so there are the 10 teams
+df["TeamName_race"] = df["TeamName_race"].replace(team_mapping)
+
+print(df["TeamName_race"].unique())
 
 df.to_csv("data/basic_combined_data_clean.csv", index=False)
---
+++ model.py
@@ -0,0 +1,4 @@
+import pandas as pd
+
+
+df = pd.read_csv("data/basic_combined_data_clean.csv")
