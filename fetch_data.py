506bf04996c7bb1cc626ed67c88c786bc2c7c8e6
AidenComiskey <aiden.whisk@hotmail.com>
2025-08-26 16:17:06 +0100

    slight edit to code to upload new data into csvs in new folder

--- fetch_data.py
+++ fetch_data.py
@@ -5,8 +5,8 @@ from main import get_year_data
 for year in range(2018, 2026):  # up to 2025
     try:
         race_df, quali_df = get_year_data(year)  # should load from cache
-        race_df.to_csv(f"race_results_{year}.csv", index=False)
-        quali_df.to_csv(f"quali_results_{year}.csv", index=False)
+        race_df.to_csv(f"enhanced_data/race_results_{year}.csv", index=False)
+        quali_df.to_csv(f"enhanced_data/quali_results_{year}.csv", index=False)
         print(f"Saved {year} data to CSV")
     except Exception as e:
         print(f"Skipping {year} due to error: {e}")
