2ddd01fe1f0b6397ae2378e2259f354d83a8bc9e
AidenComiskey <aiden.whisk@hotmail.com>
2025-08-28 15:59:43 +0100

    tried xgbregressor model and got slight improvement up to 3.448 MAE

--- model.py
+++ model.py
@@ -1,34 +1,44 @@
 import pandas as pd
-from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
+from xgboost import XGBRegressor
 from sklearn.metrics import mean_absolute_error
 
 df = pd.read_csv("enhanced_data/combined_data_clean.csv")
 
-# create label encoders to convert the data into numbers for the model to use
-le_driver = LabelEncoder()
-le_team = LabelEncoder()
-le_circuit = LabelEncoder()
-le_status = LabelEncoder()
-le_retirement = LabelEncoder()
-
-# convert drivers abbreviation, team name and circuit name to numbers
-df['Driver_to_num'] = le_driver.fit_transform(df['Abbreviation'])
-df['Team_to_num'] = le_team.fit_transform(df['TeamName_race'])
-df['Circuit_to_num'] = le_circuit.fit_transform(df['Circuit_race'])
-
 # set up what will be used to predict and what will be predicted
 X = df[
-    ['Position_quali', 'GridPosition', 'Driver_to_num', 'Team_to_num', 'Circuit_to_num','IsWetRace',
+    ['Position_quali', 'GridPosition', 'Abbreviation', 'TeamName_race', 'Circuit_race','IsWetRace',
      'IsWetQuali', 'BestLap_Sec', 'Year']]
 y = df['Position_race']
 
+
+# categorical features to be converted
+cat_features = ['Abbreviation', 'TeamName_race', 'Circuit_race']
+encoder = OneHotEncoder(handle_unknown="ignore", sparse_output=False)
+
+X_encoded = pd.DataFrame(
+    encoder.fit_transform(X[cat_features]),
+    index=X.index
+)
+
+# keep the numeric columns and join them back with the encoded values
+X_numeric = X.drop(columns=cat_features)
+X_final = pd.concat([X_numeric, X_encoded], axis=1)
+
 # split the model into test and train data
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X_final, y, test_size=0.2, random_state=42)
 
 # build the model
-model = RandomForestRegressor(n_estimators=100, random_state=42)
+model = XGBRegressor(
+    n_estimators=500,
+    learning_rate=0.05,
+    max_depth=6,
+    subsample=0.8,
+    colsample_bytree=0.8,
+    random_state=42
+)
+
 model.fit(X_train, y_train)
 
 y_pred = model.predict(X_test)
