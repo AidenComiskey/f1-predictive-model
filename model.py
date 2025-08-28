1eff09110f68e875595d2ee43ebb2f0816f3e6e9
AidenComiskey <aiden.whisk@hotmail.com>
2025-08-28 14:48:54 +0100

    created new model with better data only slight improvement to MAE of 3.48

--- model.py
+++ model.py
@@ -4,12 +4,14 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error
 
-df = pd.read_csv("data/basic_combined_data_clean.csv")
+df = pd.read_csv("enhanced_data/combined_data_clean.csv")
 
 # create label encoders to convert the data into numbers for the model to use
 le_driver = LabelEncoder()
 le_team = LabelEncoder()
 le_circuit = LabelEncoder()
+le_status = LabelEncoder()
+le_retirement = LabelEncoder()
 
 # convert drivers abbreviation, team name and circuit name to numbers
 df['Driver_to_num'] = le_driver.fit_transform(df['Abbreviation'])
@@ -17,7 +19,9 @@ df['Team_to_num'] = le_team.fit_transform(df['TeamName_race'])
 df['Circuit_to_num'] = le_circuit.fit_transform(df['Circuit_race'])
 
 # set up what will be used to predict and what will be predicted
-X = df[['Position_quali', 'GridPosition', 'Driver_to_num', 'Team_to_num', 'Circuit_to_num', 'Year']]
+X = df[
+    ['Position_quali', 'GridPosition', 'Driver_to_num', 'Team_to_num', 'Circuit_to_num','IsWetRace',
+     'IsWetQuali', 'BestLap_Sec', 'Year']]
 y = df['Position_race']
 
 # split the model into test and train data
